9144392eb4bd824893da5f21e656eeab7f9e2703
Adi Lasri <adila78@hotmail.fr>
2021-01-08 13:00:04 +0100

    Debut de js
    Ouverture du repertoire

---
+++ rectangle/java.js
@@ -0,0 +1,11 @@
+var a,b,c,d;
+
+function maFonction(){
+a = window.prompt('largeur du rectangle');
+b = window.prompt('longueur du rectangle');
+c=(a+b)*2
+d=a*b
+alert('la surface  est de: '+d);
+alert('le perimetre est de: '+c);
+}
+                                                     
