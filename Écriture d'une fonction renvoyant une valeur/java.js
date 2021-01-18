753bdef0f544c3811eccca8eb5797f13d8a6b8fd
Adi <adila78@hotmail.fr>
2021-01-18 10:11:56 +0100

    ajout de l'exercice 2 rayon

---
+++ Écriture d'une fonction renvoyant une valeur/java.js
@@ -0,0 +1,8 @@
+var a,b,c,d;
+
+function maFonction(){
+a = window.prompt('rayon du cercle');
+b=3.14*(a*a)
+alert('la surface  est de: '+b);
+}
+                                                     
