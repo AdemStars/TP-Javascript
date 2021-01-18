c1003edd30d36de5490beb552a5da845c6c8ac0a
Adi <adila78@hotmail.fr>
2021-01-18 11:34:29 +0100

    ajout de l'exercice for

---
+++ Boucles for/java.js
@@ -0,0 +1,22 @@
+function boucle(){
+    var tab = new Array(3);
+    
+	for (var i=0;i<tab.length;i++)
+					{
+						tab[i]=i*i;					
+					}
+					
+				alert(tab);
+}
+
+function boucle2(){
+    var a = parseInt(prompt('Quelle est la longueur souhaitée du tableau?'));
+    var tab = new Array();
+    
+	for (var i=0;i<a;i++)
+					{
+						tab[i]=i*i;					
+					}
+					
+    alert(tab);
+}
